1394cbcbe56a3148ce2f01b3139fea44347d98ff
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-02-24 13:43:18 -0600

    SimpleAdditions

--- arraygen.py
+++ arraygen.py
@@ -1,1 +1,9 @@
 #one function for sorted, one function for unsorted
+import random
+
+unsorted = []
+sorted = []
+
+for i in range(0,10000):
+    n = random.randint(1, 10000 + 1)
+    unsorted.append(n)
--- sorts.py
+++ sorts.py
@@ -1,1 +1,99 @@
-#INS SORTS HERE
+#Bubble Sort - Source:GeeksForGeeks
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n-1):
+    # range(n) also work but outer loop will
+    # repeat one time more than needed.
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+#Merge Sort - Source:GeeksForGeeks
+def mergeSort(arr):
+    if len(arr) > 1:
+  
+         # Finding the mid of the array
+        mid = len(arr)//2
+  
+        # Dividing the array elements
+        L = arr[:mid]
+  
+        # into 2 halves
+        R = arr[mid:]
+  
+        # Sorting the first half
+        mergeSort(L)
+  
+        # Sorting the second half
+        mergeSort(R)
+  
+        i = j = k = 0
+  
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+  
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+  
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+#Quick Sort - Source:GeeksForGeeks
+def partition(arr, low, high):
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low, high):
+ 
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+ 
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+ 
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+ 
+# Function to do Quick sort
+ 
+ 
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+ 
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+ 
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
